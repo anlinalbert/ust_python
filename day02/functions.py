de69981ac3eb4c4e453cc5c87c0263655e4de2ed
anlinalbert <anlinalbert@gmail.com>
2024-10-22 09:42:20 +0530

    added code to show usage of function

---
+++ day02/functions.py
@@ -0,0 +1,7 @@
+def find_middle_char(s):
+    """Function to find the middle character of a string."""
+    middle_index = len(s) // 2
+    return s[middle_index]
+
+
+print(find_middle_char("Superman"))
