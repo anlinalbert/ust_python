1a48ef094c2602bceec9040194208b534161231e
anlinalbert <anlinalbert@gmail.com>
2024-10-26 11:33:26 +0530

    usage of list comprehension

---
+++ day04/remove_vowels.py
@@ -0,0 +1,6 @@
+def remove_vowels(input_text):
+    """Function to remove vowels from a string."""
+    return ''.join([char for char in input_text if char not in set("aeiouAEIOU")]) # Do you know why I used a set?
+
+
+print(f"New text: {remove_vowels(input('Enter a string: '))}")
