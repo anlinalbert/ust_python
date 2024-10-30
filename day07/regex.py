54d1bf9490bc376482048452c9d61203413f5830
anlinalbert <anlinalbert@gmail.com>
2024-10-30 09:49:23 +0530

    added one more fn to capture dates

--- day07/regex.py
+++ day07/regex.py
@@ -11,7 +11,13 @@ def find_with_starting_letter(text, letter):
     return re.findall(rf"\b{letter}\w*", text, re.IGNORECASE)
 
 
+def capture_date(text):
+    """Function to capture dates from a string. Format: yyyy-mm-dd or yyyy/mm/dd."""
+    return re.findall(r"\d{4}[-/]\d{2}[-/]\d{2}", text)
+
+
 user_input = input("Enter a string: ")
 starting_letter = input("Enter starting letter: ")
 print(f"Matches: {match_numbers(user_input)}")
 print(f"Matches: {find_with_starting_letter(user_input, starting_letter)}")
+print(f"Matches: {capture_date(user_input)}")
