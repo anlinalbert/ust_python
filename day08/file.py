33433a38bef945b96fccc5a973388a81c260ecd6
anlinalbert <anlinalbert@gmail.com>
2024-11-02 10:47:27 +0530

    cleaner code

--- day08/file.py
+++ day08/file.py
@@ -2,9 +2,10 @@ def get_line_data(target_school):
     """Function to get line data from file based on input."""
     with open("sample.csv", "r") as file:
         for line in file:
-            extracted_school = line.split(",")[1]
-            if target_school == extracted_school.lower():
-                return line
+            if line.strip():
+                extracted_school = line.strip().split(",")[1]
+                if target_school == extracted_school.lower():
+                    return line
 
 
 school = input("Enter target school: ")
