1561d8dd2aeb6371a832c4fe34783e082e167181
anlinalbert <anlinalbert@gmail.com>
2024-11-02 10:49:09 +0530

    max() usage

---
+++ day08/get_max_key.py
@@ -0,0 +1,9 @@
+def max_key(user_dictionary):
+    """Function to get max key from a dictionary."""
+    return max(user_dictionary)
+
+
+# Simple dictionary
+my_dictionary = {1: 25, 2: 33, 55: 1}
+
+print(f"Max key: {max_key(my_dictionary)}")
