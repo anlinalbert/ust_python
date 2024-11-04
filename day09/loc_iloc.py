7078bb821f1791e192382927b7030576267b12c1
anlinalbert <anlinalbert@gmail.com>
2024-11-04 09:43:04 +0530

    pandas series loc & iloc

---
+++ day09/loc_iloc.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+student_series = pd.Series({"Anlin": 97, "Alice": 85, "Jock": 44, "RRR": 68})
+
+print(student_series.iloc[2])   # Based on values
+print(student_series.loc["RRR"])    # Based on index
