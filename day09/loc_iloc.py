af660fbdbd2c0248054d96ba3b6e4561786a8aed
anlinalbert <anlinalbert@gmail.com>
2024-11-04 09:49:45 +0530

    added one more print

--- day09/loc_iloc.py
+++ day09/loc_iloc.py
@@ -4,3 +4,4 @@ student_series = pd.Series({"Anlin": 97, "Alice": 85, "Jock": 44, "RRR": 68})
 
 print(student_series.iloc[2])   # Based on values
 print(student_series.loc["RRR"])    # Based on index
+print(student_series[student_series > 80])  # Students whose marks greater than 80
