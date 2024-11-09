db6802b3e561f39c63e0ae8d0be0d9e775f11ff9
anlinalbert <anlinalbert@gmail.com>
2024-11-09 12:50:26 +0530

    working out problems

--- Practice_Programs/day12/sample_workout.py
+++ Practice_Programs/day12/sample_workout.py
@@ -1,17 +1,20 @@
 import pandas as pd
 
 data = {
-    "Store": ["Store1", "Store1", "Store2", "Store2", "Store3", "Store3"],
-    "Region": ["North", "North", "South", "South", "East", "East"],
-    "Sales": [200, 150, 300, 250, 400, 350]
+    "Store": ["Store1", "Store1", "Store2", "Store2", "Store3", "Store3", "Store4", "Store5"],
+    "Region": ["North", "North", "South", "South", "East", "East", "North", "South"],
+    "Sales": [200, 150, 300, 250, 400, 350, 100, 200]
 }
 
 df = pd.DataFrame(data)
 
 # Print sum by grouping
-#print(df.groupby("Store")["Sales"].sum())
+print(df.groupby("Store")["Sales"].sum())
 region_data = df.groupby("Region")["Sales"].sum().reset_index()
-print(region_data)
 
 
-df.merge(df, region_data, on="Region", how="left")
+df = pd.merge(df, region_data, on="Region", how="left", suffixes=("_Store", "_Region"))
+print(df)
+
+df["SalesByRegion"] = df[df.groupby("Store")["Sales"].sum()] / df["Sales_Region"] * 100
+print(df)
