b7db87195616e2c1e23f51f8a7271cc331b00ef0
anlinalbert <anlinalbert@gmail.com>
2024-11-11 08:41:59 +0530

    working out problems

--- Practice_Programs/day13/titanic_exercises.py
+++ Practice_Programs/day13/titanic_exercises.py
@@ -2,5 +2,11 @@ import pandas as pd
 
 titanic = pd.read_csv(r"../datasets/titanic.csv")
 
+# Compare the average of age of passengers based on "Survived"
 print(titanic.groupby("Survived")["Age"].agg(["mean"]).reset_index())
+
+# Calculate survival rate by gender
+gender_survival = titanic.groupby("Sex")["Survived"].agg(["mean"]).reset_index()
+gender_survival["mean"] = gender_survival["mean"] * 100
+print(gender_survival)
 
