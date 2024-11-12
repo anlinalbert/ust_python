cd62b87c277c3d6a9ae4599b5e129f89dad8f44e
anlinalbert <anlinalbert@gmail.com>
2024-11-12 08:20:33 +0530

    small code improvements

--- Practice_Programs/day13/sql_demo.py
+++ Practice_Programs/day13/sql_demo.py
@@ -1,10 +1,14 @@
 import sqlite3
+from pathlib import Path
 
-connection = sqlite3.connect(r"../sql/Chinook_Sqlite.sqlite")
-curr = connection.cursor()
+# Path for better cross-platform compatibility
+db_path = Path(r"../sql/Chinook_Sqlite.sqlite")
 
-curr.execute("SELECT name FROM sqlite_master WHERE type='table'")
-tables = curr.fetchall()
+# Auto closing
+with sqlite3.connect(db_path) as connection:
+    cursor = connection.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    tables = cursor.fetchall()
 
 for table in tables:
     print(table)
