15b15977ab4a8f5df902a03a87dded5183ebd74d
anlinalbert <anlinalbert@gmail.com>
2024-11-12 08:13:26 +0530

    testing sqlite3

--- Practice_Programs/day13/sql_demo.py
+++ Practice_Programs/day13/sql_demo.py
@@ -3,4 +3,8 @@ import sqlite3
 connection = sqlite3.connect(r"../sql/Chinook_Sqlite.sqlite")
 curr = connection.cursor()
 
-print(curr)
+curr.execute("SELECT name FROM sqlite_master WHERE type='table'")
+tables = curr.fetchall()
+
+for table in tables:
+    print(table)
