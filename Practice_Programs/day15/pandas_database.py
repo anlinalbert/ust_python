6c56750b04a0d46bf268e81e70ab93eca1e4a97a
anlinalbert <anlinalbert@gmail.com>
2024-11-13 09:41:08 +0530

    added comments

--- Practice_Programs/day15/pandas_database.py
+++ Practice_Programs/day15/pandas_database.py
@@ -5,12 +5,14 @@ from pathlib import Path
 db_path = Path(r"../sql/Chinook_Sqlite.sqlite")
 
 with sqlite3.connect(db_path) as connection:
+    # Select 10 customer data
     query = """
     SELECT * FROM Customer LIMIT 10
     """
 
     customer_data = pd.read_sql_query(query, connection)
 
+    # Group by BillingCountry
     query = """
     SELECT BillingCountry, SUM(Total) as Total FROM Invoice
     GROUP BY BillingCountry
@@ -18,6 +20,7 @@ with sqlite3.connect(db_path) as connection:
 
     total = pd.read_sql_query(query, connection)
 
+    # Same as above but using pandas
     query = """
         SELECT *
         FROM Invoice
