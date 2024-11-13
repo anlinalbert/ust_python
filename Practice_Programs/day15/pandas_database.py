c73d92002411ba8f7d5a074b291d137df7e8d3f1
anlinalbert <anlinalbert@gmail.com>
2024-11-13 09:23:41 +0530

    combining pandas & sql

---
+++ Practice_Programs/day15/pandas_database.py
@@ -0,0 +1,20 @@
+import sqlite3
+import pandas as pd
+from pathlib import Path
+
+db_path = Path(r"../sql/Chinook_Sqlite.sqlite")
+
+with sqlite3.connect(db_path) as connection:
+    query = """
+    SELECT * FROM Customer LIMIT 10
+    """
+
+    customer_data = pd.read_sql_query(query, connection)
+
+    query = """
+    SELECT BillingCountry, SUM(Total) as Total FROM Invoice
+    GROUP BY BillingCountry
+    """
+
+    total = pd.read_sql_query(query, connection)
+print(total)
