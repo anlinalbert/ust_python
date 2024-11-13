00dbc35e1abe23c4e7405b2de78cd63d9cdfccfe
anlinalbert <anlinalbert@gmail.com>
2024-11-13 08:27:30 +0530

    crud operations

---
+++ Practice_Programs/day15/crud_operations.py
@@ -0,0 +1,20 @@
+import sqlite3
+from pathlib import Path
+
+db_path = Path(r"../sql/Chinook_Sqlite.sqlite")
+
+new_customer = ("A001", "Ken", "Adams", "UST", "ken.adams@ust", "USA")
+
+with sqlite3.connect(db_path) as connection:
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO Customer(CustomerId, FirstName, LastName, Company, Email, Country)
+        VALUES(?, ?, ?, ?, ?, ?), new_customer
+        """
+    )
+
+    connection.commit()
+
+print("New customer added.")
