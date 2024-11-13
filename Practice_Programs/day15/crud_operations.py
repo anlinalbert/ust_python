8aa5b5e12a9cd8a412f4bbb174afd6f726adf2e5
anlinalbert <anlinalbert@gmail.com>
2024-11-13 08:40:30 +0530

    crud operations

--- Practice_Programs/day15/crud_operations.py
+++ Practice_Programs/day15/crud_operations.py
@@ -5,16 +5,28 @@ db_path = Path(r"../sql/Chinook_Sqlite.sqlite")
 
 new_customer = (123, "Ken", "Adams", "UST", "ken.adams@ust", "USA")
 
-with sqlite3.connect(db_path) as connection:
-    cursor = connection.cursor()
+try:
+    with sqlite3.connect(db_path) as connection:
+        cursor = connection.cursor()
 
-    cursor.execute(
-        """
-        INSERT INTO Customer(CustomerId, FirstName, LastName, Company, Email, Country)
-        VALUES(?, ?, ?, ?, ?, ?)
-        """, new_customer
-    )
+        # Create
+        # cursor.execute(
+        #     """
+        #     INSERT INTO Customer(CustomerId, FirstName, LastName, Company, Email, Country)
+        #     VALUES(?, ?, ?, ?, ?, ?)
+        #     """, new_customer
+        # )
+        # print("New customer added.")
 
-    connection.commit()
+        # Read
+        customer = cursor.execute(
+            """
+            SELECT * FROM Customer
+            WHERE CustomerId = 123
+            """
+        )
+        for row in customer:
+            print(row)
 
-print("New customer added.")
+except sqlite3.Error as e:
+    print("Error!")
