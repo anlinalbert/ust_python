2c9b3b32c726007155b817f95af4e31af13fc8ec
anlinalbert <anlinalbert@gmail.com>
2024-11-16 10:59:53 +0530

    better security

--- Practice_Programs/day16/mongodb.py
+++ Practice_Programs/day16/mongodb.py
@@ -1,7 +1,10 @@
+import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
 
-uri = r"mongodb+srv://rohithrajreganti:rohithrajreganti@cluster0.rd7dn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+load_dotenv()
+uri = os.getenv("MONGODB_URI")
 
 # Create a client and connect to the server
 client = MongoClient(uri, server_api=ServerApi("1"), tlsAllowInvalidCertificates=True)
